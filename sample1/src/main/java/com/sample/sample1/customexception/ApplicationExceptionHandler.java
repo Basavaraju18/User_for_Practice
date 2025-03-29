0bb309843c5c250682492c0b7341b1ae61a17786
bassavaraju18 <basavarajuds184@gmail.com>
2025-03-29 12:12:22 +0530

    Updated Error reponse of geting user id whhcih is not present

--- sample1/src/main/java/com/sample/sample1/customexception/ApplicationExceptionHandler.java
+++ sample1/src/main/java/com/sample/sample1/customexception/ApplicationExceptionHandler.java
@@ -1,14 +1,26 @@
 package com.sample.sample1.customexception;
+import java.time.LocalDateTime;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.sample.sample1.dto.ErrorResponse;
+
 @RestControllerAdvice
 public class ApplicationExceptionHandler  {
 
+	
+	
+//	 public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+//	        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
+//	    }
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
-        return new ResponseEntity<String>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleUserNotFoundException(UserNotFoundException ex) {
+    	ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage(), LocalDateTime.now());
+    	
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        
     }
 }
