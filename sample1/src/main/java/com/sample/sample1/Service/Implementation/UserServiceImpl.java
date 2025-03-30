e08bf560bf825726aedc135d2ed1079efdec4333
bassavaraju18 <basavarajuds184@gmail.com>
2025-03-30 14:17:21 +0530

    Modified ServiceImpl.java

--- sample1/src/main/java/com/sample/sample1/Service/Implementation/UserServiceImpl.java
+++ sample1/src/main/java/com/sample/sample1/Service/Implementation/UserServiceImpl.java
@@ -1,6 +1,10 @@
 package com.sample.sample1.Service.Implementation;
 
-import org.hibernate.validator.internal.util.logging.LoggerFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
@@ -10,14 +14,6 @@ import com.sample.sample1.Repository.UserRepository;
 import com.sample.sample1.Service.UserService;
 import com.sample.sample1.customexception.UserNotFoundException;
 import com.sample.sample1.dto.UserResponse;
-
-import org.slf4j.Logger;
-
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @Validated
 @Service
